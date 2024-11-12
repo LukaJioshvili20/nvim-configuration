77bd461f8c93399e4a168eb798ad977c3042263d
LukaJioshvili <luka.jioshvili20@gmail.com>
2024-11-12 17:08:14 +0100

    bugfix: fix tree sitter syntax error

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,9 +1,10 @@
 return {
-  run = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = { "css", "scss" }, -- add other languages if needed
-      sync_install = false,
+      sync_install = true,
       auto_install = true,
       ignore_install = {}, -- Add an empty table to prevent errors
       highlight = {
