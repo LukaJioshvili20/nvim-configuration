7c0cbcf486856b94af5a0158e37b130456ba9d81
LukaJioshvili <luka.jioshvili20@gmail.com>
2024-11-12 11:14:34 +0100

    feat: add css and scss support for front-end project

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -50,6 +50,23 @@ require("lazy").setup({
       },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "css", "scss" }, -- add other languages if needed
+        sync_install = false,
+        auto_install = true,
+        ignore_install = {}, -- Add an empty table to prevent errors
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false, -- Optional, can disable additional Vim regex
+        },
+      })
+    end,
+  },
+  { "ap/vim-css-color" },
 })
 
 require("project_nvim").setup({
